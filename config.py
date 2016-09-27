c9e7d6a34604cabe214eeffc8d1231ad512ffeff
mkfsn <p408865@gmail.com>
2016-09-27 20:47:00 +0900

    Set default of SQLALCHEMY_TRACK_MODIFICATIONS to True

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ __author__ = 'mkfsn'
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'osaka-u.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 # SQLALCHEMY_ECHO = True
